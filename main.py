6762760cf76a3d2bdf729fdd492ad60e753407ed
Renat Gubaydullin <renatgubaudullin@gmail.com>
2021-11-19 15:17:31 +0300

    work

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QBrush
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+import random
 
 
-# Press the green button in the gutter to run the script.
+class MyWidget(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.dr = False
+        self.pushButton.clicked.connect(self.on_click)
+
+    def paintEvent(self, e):
+        super().paintEvent(e)
+        if self.dr:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+
+    def draw(self, qp):
+        pos = [random.randint(0, 300) for i in range(2)]
+        size = [random.randint(10, 100)] * 2
+        qp.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        qp.drawEllipse(*pos, *size)
+        pos = [random.randint(100, 300) for i in range(2)]
+        size = [random.randint(10, 100)] * 2
+        qp.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        qp.drawEllipse(*pos, *size)
+        pos = [random.randint(200, 300) for i in range(2)]
+        size = [random.randint(10, 100)] * 2
+        qp.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        qp.drawEllipse(*pos, *size)
+
+    def on_click(self):
+        self.pushButton.setVisible(False)
+        self.dr = True
+        self.update()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
